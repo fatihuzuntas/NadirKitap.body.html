e457b2d0b81ad34b2751a38089db6bc0dbeb986d
fatihuzuntas <73489872+fatihuzuntas@users.noreply.github.com>
2022-12-07 13:55:17 +0300

    son eklenenler component design
    bu kısımda wson eklenenler kısmı eklendi

---
+++ soneklenenlercomponent.vue
@@ -0,0 +1,212 @@
+<template>
+ <div>
+    <div><a href="" title="Yeni eklenen kitaplar"><h4 class="son-eklenenler-class" ><span  aria-hidden="true" style="margin-right:5px"></span>&#128213; Son Eklenenler</h4></a></div>
+<div class="son-eklenen-kitap-listesi">
+  <div class="Dördüncü-Dilek-foto"><a style="" href="" title="Dördüncü Dilek - La Disparition Emre Ergin"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/449685/29/Kitap_202211091713094496859.jpg">
+  </a></div>
+<div class="Dördüncü-Dilek-yazi"><a style="text-decoration: none;color:#63b5ea;" href="" title="Dördüncü Dilek - La Disparition Emre Ergin">Dördüncü Dilek <br/>- La Disparition</a><br/>55.46₺</div>
+<div class="interpol-lütfi-çiçek-foto"><a href="" title="Bilinmeyen Yönleriyle İnterpol lütfi çiçek"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/5001/29/Kitap_20221109171337500110.jpg">
+</a></div>
+<div class="interpol-lütfi-çiçek-yazi"><a style="text-decoration: none;color:#63b5ea"  href="" title="Bilinmeyen Yönleriyle İnterpol lütfi çiçek">Bilinmeyen<br />Yönleriyle<br /> İnterpol</a><br/>82.51₺</div>
+<div class="TAPINAK-ŞÖVALYELERİ-foto" ><a href="" title="TAPINAK ŞÖVALYELERİ PIERS PAUL READ"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/464383/29/Kitap_2022110917133146438312.jpg">
+</a></div>
+<div class="TAPINAK-ŞÖVALYELERİ-yazi"><a style="text-decoration: none;color:#63b5ea"  href="" title="TAPINAK ŞÖVALYELERİ PIERS PAUL READ">TAPINAK<br /> ŞÖVALYELERİ</a><br/>19.50₺</div>
+<div class="MİLLİYETÇİLİK-ÜLKÜCÜLÜK-foto"><a href="" title="MİLLİYETÇİLİK ÜLKÜCÜLÜK AYDINLAR NECMETTİN HACIEMİNOĞLU"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/489051/29/Kitap_202211091712524890518.jpg">
+</a></div>
+<div class="MİLLİYETÇİLİK-ÜLKÜCÜLÜK-yazi"><a style="text-decoration: none;color:#63b5ea"  href="" title="MİLLİYETÇİLİK ÜLKÜCÜLÜK AYDINLAR NECMETTİN HACIEMİNOĞLU">MİLLİYETÇİLİK<br /> ÜLKÜCÜLÜK<br /> AYDINLAR</a><br/>65.30₺</div>
+<div class="Sir-Hunt-John-foto"><a href="" title="THE ASCENT OF EVEREST Général Sir Hunt John"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/524653/29/Kitap_2022110917130852465311.jpg">
+</a></div>
+<div class="Sir-Hunt-John-yazi"><a style="text-decoration: none;color:#63b5ea"   href="" title="THE ASCENT OF EVEREST Général Sir Hunt John">THE ASCENT<br />OF<br /> EVEREST</a><br/>12.20₺</div>
+<div class="Devlet-Doğu-Perinçek-foto"><a href="" title="Osmanlı'dan Bugüne Toplum ve Devlet Doğu Perinçek"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/340270/29/Kitap_202211091713103402707.jpg">
+</a></div>
+<div class="Devlet-Doğu-Perinçek-yazi"><a style="text-decoration: none;color:#63b5ea"  href="" title="Osmanlı'dan Bugüne Toplum ve Devlet Doğu Perinçek">Osmanlı'dan<br /> Bugüne<br />Toplum ve Devlet</a><br/>102.50₺</div>
+<div class="Charles-Darwin-foto"><a href="" title="İnsanda ve Hayvanlarda Duyguların İfade Edilmesi Charles Darwin"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/1343458/29/Kitap_2022110917125813434581.jpg">
+</a></div>
+<div class="Charles-Darwin-yazi"><a style="text-decoration: none;color:#63b5ea"  href="" title="İnsanda ve Hayvanlarda Duyguların İfade Edilmesi Charles Darwin">İnsanda ve<br /> Hayvanlarda<br />Duyguların<br /> İfade Edilmesi</a><br/>10.00₺</div>
+<div class="Sefa-Saygılı-foto"><a href="" title="Çocuklarda Davranış Bozuklukları Sefa Saygılı"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/570411/29/Kitap_202211091712395704114.jpg">
+</a></div>
+<div class="Sefa-Saygılı-yazi"><a style="text-decoration: none;color:#63b5ea"  href="" title="Çocuklarda Davranış Bozuklukları Sefa Saygılı">Çocuklarda<br />Davranış<br />Bozuklukları</a><br/>75.15₺</div>
+<div class="Çeyiz-foto"><a href="" title="Çeyiz Hatice Kübra Tongar"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/23201/29/Kitap_202211011659162320113.jpg">
+</a></div>
+<div class="Çeyiz-yazi"><a style="text-decoration: none;color:#63b5ea"  href="" title="Çeyiz Hatice Kübra Tongar">Çeyiz</a><br/>25.46₺</div>
+<div class="Hany-Mehanna-foto"><a href="" title="The Miracles Of The Seven Dances (Plak) Hany Mehanna"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/1166951/29/Efemera_2022110917124711669513.jpg">
+</a></div>
+<div class="Hany-Mehanna-yazi"><a style="text-decoration: none;color:#63b5ea"  href="" title="The Miracles Of The Seven Dances (Plak) Hany Mehanna">The Miracles <br />Of The <br />Seven Dances <br />(Plak) Hany Mehan</a><br/>35.89₺</div>
+<div class="ALLAHIN-VARLIĞI-foto"><a href="" title="ALLAH'IN VARLIĞI BEKİR TOPALOĞLU"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/240804/29/Kitap_202211091712552408048.jpg">
+</a></div>
+<div class="ALLAHIN-VARLIĞI-yazi"><a style="text-decoration: none;color:#63b5ea"   href="" title="ALLAH'IN VARLIĞI BEKİR TOPALOĞLU">ALLAH'IN VARLIĞI</a><br/>43.25₺</div>
+<div class="FULL-BLAST-foto"><a href="" title="FULL BLAST 3 FOR TURKEY WORKBOOK + GRAMMAR BOOK + STUDENTS BOOK / 2 CİLT TAKIM"><img style="height:100%;" src="https://static.nadirkitap.com/fotograf_t/19940/29/Kitap_20221109171249199409.jpg">
+</a></div>
+<div class="FULL-BLAST-yazi"><a style="text-decoration: none;color:#63b5ea"  href="" title="FULL BLAST 3 FOR TURKEY WORKBOOK + GRAMMAR BOOK + STUDENTS BOOK / 2 CİLT TAKIM">FULL BLAST 3<br /> FOR TURKEY<br /> WORKBOOK +<br /> GRAMMAR BOOK +</a><br/>85.35₺</div></div>
+<div class="son-eklenen-kitaplar-listesi-box"></div>
+ </div>
+</template>
+<script setup>
+
+</script>
+<style >
+    .son-eklenenler-class{
+  position:absolute;
+  top: 710px;
+  left: 450px;
+  font-size:20px ;
+  color: #931818;
+}
+
+.Dördüncü-Dilek-foto{
+  position:absolute;
+  top: 800px;
+  left: 480px;
+  
+}
+
+.Dördüncü-Dilek-yazi{
+  position:absolute;
+  top: 950px;
+  left: 480px;
+  
+}
+
+.interpol-lütfi-çiçek-foto{
+  position:absolute;
+  top: 800px;
+  left: 680px;
+}
+
+.interpol-lütfi-çiçek-yazi{
+  position:absolute;
+  top: 950px;
+  left: 690px;
+}
+
+.TAPINAK-ŞÖVALYELERİ-foto{
+  position:absolute;
+  top: 800px;
+  left: 880px;
+}
+
+.TAPINAK-ŞÖVALYELERİ-yazi{
+  position:absolute;
+  top: 950px;
+  left: 880px;
+}
+
+.MİLLİYETÇİLİK-ÜLKÜCÜLÜK-foto{
+  position:absolute;
+  top: 800px;
+  left: 1080px;
+}
+
+.MİLLİYETÇİLİK-ÜLKÜCÜLÜK-yazi{
+  position:absolute;
+  top: 950px;
+  left: 1070px;
+}
+
+.Sir-Hunt-John-foto{
+  position:absolute;
+  top: 1070px;
+  left: 480px;
+}
+
+.Sir-Hunt-John-yazi{
+  position:absolute;
+  top: 1225px;
+  left: 480px;
+}
+
+.Devlet-Doğu-Perinçek-foto{
+  position:absolute;
+  top: 1070px;
+  left: 680px;
+}
+
+.Devlet-Doğu-Perinçek-yazi{
+  position:absolute;
+  top: 1225px;
+  left: 670px;
+}
+
+.Charles-Darwin-foto{
+  position:absolute;
+  top: 1070px;
+  left: 880px;
+}
+
+.Charles-Darwin-yazi{
+  position:absolute;
+  top: 1225px;
+  left: 885px;
+}
+
+.Sefa-Saygılı-foto{
+  position:absolute;
+  top: 1070px;
+  left: 1080px;
+}
+
+.Sefa-Saygılı-yazi{
+  position:absolute;
+  top: 1225px;
+  left: 1090px;
+}
+
+.Çeyiz-foto{
+  position:absolute;
+  top: 1340px;
+  left: 480px;
+}
+
+.Çeyiz-yazi{
+  position:absolute;
+  top: 1490px;
+  left: 510px;
+}
+
+.Hany-Mehanna-foto{
+  position:absolute;
+  top: 1340px;
+  left: 680px;
+}
+
+.Hany-Mehanna-yazi{
+  position:absolute;
+  top: 1490px;
+  left: 680px;
+}
+
+.ALLAHIN-VARLIĞI-foto{
+  position:absolute;
+  top: 1340px;
+  left: 880px;
+}
+
+.ALLAHIN-VARLIĞI-yazi{
+  position:absolute;
+  top: 1490px;
+  left: 860px;
+}
+
+.FULL-BLAST-foto{
+  position:absolute;
+  top: 1340px;
+  left: 1080px; 
+}
+
+.FULL-BLAST-yazi{
+  position:absolute;
+  top: 1490px;
+  left: 1070px;
+}
+
+.son-eklenen-kitaplar-listesi-box{
+  position: absolute;
+  top: 780px ;
+  left: 440px;
+  width: 780px;
+  height: 820px;
+  background-color:#faf7ec;
+  z-index: -1;
+  border: 2px dotted #eee4cd;
+}
+</style>
