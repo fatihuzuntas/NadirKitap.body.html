d43ba942d2c50858b4c1d653577a7ef2ff27dcf9
fatihuzuntas <73489872+fatihuzuntas@users.noreply.github.com>
2022-12-07 13:56:17 +0300

    telif yazısı component design
    telif yazısı yerine yerleştirildi

---
+++ telifyazisicomponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="telifyazisi">
+
+<div id="myAlert" style="text-align: center; font-size: 12px;" class="box-login" role="alert">
+  
+<p style="position:absolute;top:194%;left:30%;">2007-2022 Nadirkitap Bilişim ve Reklamcılık A.Ş. Tüm Hakları Saklıdır. Bu siteye üye olanlar Kullanıcı Sözleşmesini okumuş ve kabul etmiş sayılırlar.<br/><strong>Bilgilendirme!</strong>
+Çerezler NadirKitap.com deneyiminizi iyileştirmek için kullanılmaktadır</p> 
+    </div></div>
+</template>
+<script setup>
+
+</script>
+<style>
+    
+</style>
