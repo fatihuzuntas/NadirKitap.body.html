5359309d622f3c862d154dd0078dd42a4177720c
fatihuzuntas <73489872+fatihuzuntas@users.noreply.github.com>
2022-12-07 13:57:18 +0300

    yukarı buton component design
    tuşa basıldığında sayfanın yukarısına getirme tuşu

---
+++ yukarç-asagi-buton.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="button">
+        <a  href=""><img style="position:absolute;top:195%;" title="Yukarı Çık" src="https://static.nadirkitap.com/img/icon/yukari.png"><img/></a>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style>
+ #button {
+  display: inline-block;
+  background-color: #FF9800;
+  width: 50px;
+  height: 50px;
+  text-align: center;
+  border-radius: 4px;
+  margin: 30px;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  transition: background-color .3s;
+  z-index: 1000;
+
+}
+#button:hover {
+  cursor: pointer;
+  background-color: #333;
+}
+#button:active {
+  background-color: #555;
+}  
+
+#button::after {
+  content: "\f077";
+  font-family: FontAwesome;
+  font-weight: normal;
+  font-style: normal;
+  font-size: 2em;
+  line-height: 50px;
+  color: #fff;
+}
+
+
+</style>
