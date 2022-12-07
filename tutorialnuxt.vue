a529f4576715f1c0654c049f1f31c10f2cbf49d1
fatihuzuntas <73489872+fatihuzuntas@users.noreply.github.com>
2022-12-07 14:02:19 +0300

    nuxt.js compononet is that tutorial application

---
+++ tutorialnuxt.vue
@@ -0,0 +1,22 @@
+<!-- Please remove this file from your project -->
+<template>
+    <div id="telifyazisi">
+
+<div id="myAlert" style="text-align: center; font-size: 12px;" class="box-login" role="alert">
+  
+<p>2007-2022 Nadirkitap Bilişim ve Reklamcılık A.Ş. Tüm Hakları Saklıdır. Bu siteye üye olanlar Kullanıcı Sözleşmesini okumuş ve kabul etmiş sayılırlar.<br/><strong>Bilgilendirme!</strong>
+Çerezler NadirKitap.com deneyiminizi iyileştirmek için kullanılmaktadır</p> 
+    </div></div> 
+
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+p{
+  position:absolute;top:194%;left:30%;
+}
+
+</style>
