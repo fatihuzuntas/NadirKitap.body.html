a247aaadfcf05346f653a6f7dc7b2fda86296cd3
fatihuzuntas <73489872+fatihuzuntas@users.noreply.github.com>
2023-02-02 03:33:19 +0300

    index
    starting page

---
+++ index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div>
+      
+<blogyazilarinuxt></blogyazilarinuxt>
+<altcubuknuxt></altcubuknuxt>
+<headernuxt></headernuxt>
+<islemrehberinuxt></islemrehberinuxt>
+<tutorialnuxt></tutorialnuxt>
+<kitaplarkategorinuxt></kitaplarkategorinuxt>
+<searchnuxt></searchnuxt>
+<slidernuxt></slidernuxt>
+<soneklenenlernuxt></soneklenenlernuxt>
+<yukariasagibutonnuxt></yukariasagibutonnuxt>
+
+
+</div>
+    
+<div>
+    
+    <button class="button123" @click="signOut" v-if="firebaseUser">Sign out</button>
+    <div v-if="firebaseUser">
+      
+    </div>
+    <div v-else>User is signed out</div>
+  </div>
+
+    <h1 class="hosgeldin">hosgeldin {{ word }}</h1>
+  </div>
+</template>
+
+<script setup>
+const firebaseUser = useFirebaseUser();
+
+const signOut = async () => {
+  await signOutUser();
+};
+
+
+
+</script>
+
+
+<style>
+.hosgeldin{
+  position: absolute;
+  top: 0px;
+}
+
+</style>
+
+
